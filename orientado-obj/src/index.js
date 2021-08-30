e67370676bc22f0af73e1b729063a92ff88917a1
Josan Johnata <josanjohnata@gmail.com>
2021-08-30 11:09:54 -0300

    Programação Orietada a Objetos - Estrutura de um Obj

---
+++ orientado-obj/src/index.js
@@ -0,0 +1,6 @@
+const ObjJavascript = require('./literals/objeto');
+const ObjJson = require('./literals/objeto.json')
+
+console.log(ObjJavascript.getFinalPrice());
+
+console.log(ObjJson.produto[0].name);
