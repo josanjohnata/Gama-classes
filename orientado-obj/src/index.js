18fc2d7b3509f7af10a37f494a9e052f082d00e5
Josan Johnata <josanjohnata@gmail.com>
2021-08-30 12:03:32 -0300

    Programação Orietada a Objetos - Estrutura de uma Class

--- orientado-obj/src/index.js
+++ orientado-obj/src/index.js
@@ -1,6 +1,12 @@
 const ObjJavascript = require('./literals/objeto');
-const ObjJson = require('./literals/objeto.json')
+const ObjJson = require('./literals/objeto.json');
+// const Pessoa = require('./class/anatonia')
+const SegundaPessoa = require('./class/anatonia')
 
-console.log(ObjJavascript.getFinalPrice());
+// console.log(ObjJavascript.getFinalPrice());
 
-console.log(ObjJson.produto[0].name);
+// console.log(ObjJson.produto[0].name);
+
+// console.log(Pessoa.hello('Josan Johnata', 'dev', 29));
+
+console.log(SegundaPessoa.name);
